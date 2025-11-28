e8ff916fe2507fddd52d51b3fa8fce3a5f271460
AnkerMax <philippadam2536@gmail.com>
2025-11-28 13:18:35 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,6 @@ sudo apt-get install -y php-cli php-xml
 
 sudo bash install-sh
 
-printf "y\nn\ny\nn\nn\nn\ny\n" | sudo phoronix-test-suite batch-setup
+printf "y\nn\ny\nn\ny\ny\ny\n" | sudo phoronix-test-suite batch-setup
 sudo phoronix-test-suite install coremark cachebench compress-7zip scimark2 openssl byte npb fftw
 sudo phoronix-test-suite batch-benchmark coremark cachebench compress-7zip scimark2 openssl byte npb fftw
