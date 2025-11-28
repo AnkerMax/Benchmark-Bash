40b3b345d0f97bcfa24a83e9ac7d0ecf2cefb8f4
AnkerMax <philippadam2536@gmail.com>
2025-11-28 12:50:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+set -e  # Skript stoppt bei Fehler
 
 git clone https://github.com/phoronix-test-suite/phoronix-test-suite.git
+cd phoronix-test-suite
+
 sudo apt-get update
 sudo apt-get install -y php-cli php-xml
-cd phoronix-test-suite
+
 sudo bash install-sh
-printf "y\nn\ny\nn\nn\nn\ny\n" | phoronix-test-suite batch-setup
-phoronix-test-suite install coremark cachebench compress-7zip scimark2 openssl byte npb fftw
-phoronix-test-suite batch-benchmark coremark cachebench compress-7zip scimark2 openssl byte npb fftw
+
+printf "y\nn\ny\nn\nn\nn\ny\n" | sudo phoronix-test-suite batch-setup
+sudo phoronix-test-suite install coremark cachebench compress-7zip scimark2 openssl byte npb fftw
+sudo phoronix-test-suite batch-benchmark coremark cachebench compress-7zip scimark2 openssl byte npb fftw
